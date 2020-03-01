92fa5f0b15bbb7d41c8c937c11dd10a80c991ed7
Jessica <pan.jessica216@gmail.com>
2020-02-29 17:49:40 -0800

    added button to add food

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,39 +1,70 @@
-import React, { useState } from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
-
+import React, { useState } from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
 
 const Cell = ({ letter }: { letter: string }) => {
-    return <View style={styles.cell}>
-        <Text>{`cell ${letter}`}</Text>
+  return (
+    <View style={styles.cell}>
+      <Text>{`cell ${letter}`}</Text>
     </View>
-}
+  );
+};
 
 const Home = () => {
-    const [clicks, setClicks] = useState(0);
-    return <View style={styles.page}>
+  const [clicks, setClicks] = useState(0);
+  const [fridgeFood, setFoodNum] = useState(0);
+  return (
+    <View>
+      <View style={styles.page}>
         <Text>
-            Home Page {clicks}
+          Home Page {clicks} AHHHHHHH I'M JESSICA!!!{" "}
+          {"\nNumber of food in fridge:"} {fridgeFood}
         </Text>
-        {['a', 'b', 'c'].map((cell, i) => <Cell key={i} letter={cell}/>)}
+        {["a", "b", "c"].map((cell, i) => (
+          <Cell key={i} letter={cell} />
+        ))}
 
-        <Button title='button' onPress={() => {
-            setClicks(clicks + 1)
-        }} />
+        <Button
+          title="button"
+          onPress={() => {
+            setClicks(clicks + 1);
+          }}
+        />
+      </View>
+
+      <View style={styles.addFood}>
+        <Button
+          title="button"
+          onPress={() => {
+            setFoodNum(fridgeFood + 1);
+          }}
+        />
+      </View>
     </View>
-}
+  );
+};
 
 const styles = StyleSheet.create({
-    page: {
-        paddingTop: 15,
-        display: 'flex',
-        alignItems: 'center',
-        width: '100%',
-        minHeight: '100%',
-        backgroundColor: 'white'
-    },
-    cell: {
-        height: 40
-    }
-
+  page: {
+    paddingTop: 15,
+    display: "flex",
+    alignItems: "center",
+    width: "100%",
+    minHeight: "100%",
+    backgroundColor: "white"
+  },
+  cell: {
+    height: 40
+  },
+  addFood: {
+    flex: 1,
+    // backgroundColor: "lightblue",
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "flex-end",
+    position: "absolute",
+    width: "100%",
+    paddingRight: 20,
+    bottom: 55
+  }
 });
-export default Home; 
+export default Home;
